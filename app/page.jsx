a3ebec4343d12c1f3425287de1586d1255584749
Mohit Sharma <mr.mohit.sharma@outlook.com>
2026-07-07 09:48:57 +1000

    Property page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className="text-2xl">Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link
+        href={{
+          pathname: "properties",
+        }}
+      >
+        Go To Properties
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
