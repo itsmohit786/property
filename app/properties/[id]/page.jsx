2a8808a0f044869c310ae1ca238c8ad00ff13352
Mohit Sharma <mr.mohit.sharma@outlook.com>
2026-07-13 12:35:16 +1000

    Added DB config and changes to HomeProperties, Properties and connection in single property page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,12 @@
-const PropertyPage = () => {
-  return <div>Property Page</div>;
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+const PropertyPage = async ({ params }) => {
+  const { id } = await params;
+  await connectDB();
+  const property = await Property.findById(id).lean();
+  console.log(`PROPERTY: `, property);
+  return <div>Property Page: {property.name}</div>;
 };
 
 export default PropertyPage;
