a69d4280c53f75a46f65ccbbe25a6c4e3c6d1a3f
Mohit Sharma <mr.mohit.sharma@outlook.com>
2026-07-13 22:20:23 +1000

    added property detail main section

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,12 +3,13 @@ import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = async ({ params }) => {
   const { id } = await params;
   await connectDB();
   const property = await Property.findById(id).lean();
-  console.log(`PROPERTY: `, property);
+
   return (
     <>
       <PropertyHeaderImage image={property.images[0]}></PropertyHeaderImage>
@@ -23,10 +24,10 @@ const PropertyPage = async ({ params }) => {
         </div>
       </section>
 
-      <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6">
-          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            Property Info!
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
