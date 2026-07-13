ac189f7bc5b77c2e2caeaa3bf1a95ae789288ccd
Mohit Sharma <mr.mohit.sharma@outlook.com>
2026-07-13 20:45:25 +1000

    updated single property page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,37 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 const PropertyPage = async ({ params }) => {
   const { id } = await params;
   await connectDB();
   const property = await Property.findById(id).lean();
   console.log(`PROPERTY: `, property);
-  return <div>Property Page: {property.name}</div>;
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]}></PropertyHeaderImage>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Properties
+          </Link>
+        </div>
+      </section>
+
+      <section class="bg-blue-50">
+        <div class="container m-auto py-10 px-6">
+          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            Property Info!
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
